ef65d319d83a989e2a025ef5e2bca04a8498db02
nissirosenberg <nissirosenberg@gmail.com>
2023-01-05 17:23:36 -0500

    Art Lessons

--- ArtGroup.sql
+++ ArtGroup.sql
@@ -1,1 +1,68 @@
+/*
 "Nissi's Art" 
+I’m an art teacher, I give art lessons every Sunday and I also sell supplies to my students. I need:
+The girls’ names, their telephone number, which group they’re in, how many lessons they already came, the total cost (they pay per lesson) they have to pay for their lessons.
+In the beginning, they each need to buy a supplies package, depending on what she is learning. So far, I have 2 packages: one for painting and one for pastels. The painting
+ package is $70, and the pastel package is $12. 
+ I also want to know how much I paid for each package so that I can see how much profit I made.
+ Then I need for each girl: their full price they need to pay, how much she paid, and how much she still owes(for lessons and supplies together.) 
+ I also want to see if she is still current, if yes, then put in the column a *, and if she’s not current anymore then give her a -.
+
+ What I need in reports:
+  1) I need a a report that separates the girls between the groups they're in. I want it to be separate tables.
+  2) Total of how much profit I'm making(after expenses of supplies), total of how much I'm owed, and total of how much I was paid already
+  3) Order the girls that I see first all the girls in the pastel group, then from the paint group(doesn't matter which one), then sort them by who came the most lessons and who came the least.
+  4) How many students do I currently have?
+  5) I want a report of who is on top of payment and who's not. If a girl owes more than half of the total she needs to pay, then she's not on top of it.
+
+How much do you charge per lesson? 
+For paint I charge $32.
+For pastel I charge $15
+
+How much did you pay for each package of supplies?
+Paint $50
+Pastel $7
+
+Is there a limit to how many lessons a girl can come?
+No.
+
+How do you name your groups? 
+I have 3 groups: Pastel, Paint 1, Paint 2
+
+Can one girl do both?
+Yes, but if she decides to do both, she goes into the system twice. Once for one group and once for the other
+
+When did you start giving classes?
+In January 2022
+
+Do you ever give lessons or supplies for free?
+No. 
+
+Can a girl pay more than she owes, let’s say, advance payment?
+No, I tell them to pay me when they owe me.
+
+
+Sample data:
+Ratzi Kalman, 845.352.7694, Paint 1,  7 lessons, paid $200, Current
+Chumi Sanders, 845.912.3569, Paint 1, 16 lessons, paid $0, Current
+Goldy Morris, 845.646.3522, Pastel, 7 lessons, paid $90, Current
+Hendy Morris, 845.646.3522, Pastel, 7 lessons, paid $0, Not current
+Noami Nachman, 845.425.4521, Paint 2, 9 lessons, paid $210, Current
+Libby Glick, 845.378.4266, Paint 2, 8 lesson, paid $150, Not current
+Malky Nachman, 845.425.4521, Paint 2, 8 lessons, paid $100, not current
+Chana Ruchy Kromarsky, 845.656.3255, Pastel, 6 lessons, paid $98, current
+Roizy Glick, 845.378.4266, Paint 1, 10 lessons, paid $250, current
+Esty Davidson, 845.323.2582, Pastel, 8 lessons, Paid $60, current
+Miriam Rosner, 845.956.5689, Paint 1, 9 lessons, Paid $200, current
+Miri Rabinowitz, 718.323.4572, Paint 1, 12 lessons, Paid $170, current
+Rachy Godinger, 845.256.3487, Pastel, 4 lessons, Paid $50, current
+Zeldy Wienreb, 845.659.2541, Pastel, 6 lessons, $100, current
+Malka Lowinger, 845.753.6498, Paint 2, 8 lessons, Paid $150, current
+Esther Pollak, 845.325.1479, Paint 2, 12 lessons, Paid 260, current
+Etty Reich, 845.659.3274, Paint 2, 8 lessons, Paid $225, not current
+Devorah Schick, 845.693.2457, 5 lessons, Paid $65, current
+Shuly Frankel, 845.236.4528, 8 lessons, $100, not current
+
+
+Thank you
+*/
